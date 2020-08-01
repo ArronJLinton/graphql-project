cced51d094496864ae061e43a39cba5570b9f5cf
Arron Linton <lintonarron@gmail.com>
2020-08-01 18:14:55 -0400

    setup intial server files

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(8080, () => {
+    console.log('Listening for requests on port 8080');
+});
