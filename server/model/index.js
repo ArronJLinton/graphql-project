3daec016cd27d622eca51d0a17cb60e52187dacc
Arron Linton <lintonarron@gmail.com>
2020-08-02 14:08:23 -0400

    removed server folder

--- server/model/index.js
+++
@@ -1,5 +0,0 @@
-const User = require('./user');
-const Hobby = require('./hobby');
-const Post = require('./post');
-
-module.exports = {User, Hobby, Post}
--- server/model/post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const postSchema = new Schema({
-    comment: String,
-    userId: String
-});
-
-module.exports = mongoose.model('Post', postSchema);
